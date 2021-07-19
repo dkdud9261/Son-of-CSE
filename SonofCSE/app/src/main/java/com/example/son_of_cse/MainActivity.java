cfe24bfdcbf824e80fba9342624851663bbbfca8
dkdud9261 <ayoganatanatda@naver.com>
2021-07-20 00:41:20 +0900

    the first test

---
+++ SonofCSE/app/src/main/java/com/example/son_of_cse/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.son_of_cse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
